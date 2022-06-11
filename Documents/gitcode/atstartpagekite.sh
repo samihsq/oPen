f49a63b0bdf83c907431eb6e4e5fdeb65d8b1b0f
Samih Qureshi <samih.qureshi@gmail.com>
2022-06-10 21:37:55 -0700

    changed sh files
    put v2 then pagekite in sudo crontab

---
+++ Documents/gitcode/atstartpagekite.sh
@@ -0,0 +1,2 @@
+#cd
+sudo python /home/autopen/pagekite.py --optfile=/home/autopen/.pagekite.rc 8080 pen1.o-pen.org
--- Documents/gitcode/atstartv2.sh
+++ Documents/gitcode/atstartv2.sh
@@ -1,5 +1,3 @@
+sudo motion -b
 cd /usr/local/openresty/nginx
 sudo /usr/local/openresty/nginx/sbin/nginx -p `pwd`/ -c /usr/local/openresty/nginx/conf/nginx.conf &
-cd
-sudo motion &
-sudo python /home/autopen/pagekite.py --optfile=/home/autopen/.pagekite.rc 8080 pen1.o-pen.org
